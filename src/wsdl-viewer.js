a5c24296a1a0739ac6fdb4a11b75c552f2b6a649
Jonas Marklén <jonas.marklen@sitedirect.se>
2020-12-31 02:53:45 +0100

    formatting

--- src/wsdl-viewer.js
+++ src/wsdl-viewer.js
@@ -1,31 +1,26 @@
-function pagingInit()
-{
-	let nav = document.getElementById('nav'),
-		tabs = nav.getElementsByTagName('li'),
-		aa;
-	console.log('Paging init!');
-	for(let i = 0; i < nav.length; ++i) {
-		tabs[i].className = 'close';
-		aa = nav[i].getElementsByTagName('a')
-		//aa.onclick = function() { activate(this.href); return false; }
-		if ( i === 0 ) {
-			aa.className = 'current';
-		}
-	}
-	console.log('DONE!');
+function pagingInit() {
+    let nav = document.getElementById('nav'),
+        tabs = nav.getElementsByTagName('li'),
+        aa;
+    console.log('Paging init!')
+    for (let i = 0; i < nav.length; ++i) {
+        tabs[i].className = 'close'
+        aa = nav[i].getElementsByTagName('a')
+        //aa.onclick = function() { activate(this.href); return false; }
+        if (i === 0) {
+            aa.className = 'current'
+        }
+    }
+    console.log('DONE!')
 }
 
-function pagingActivate(name)
-{
-// 	var page
-//   if (curHeader.className=="close")
-//   {
-//     curHeader.className="";
-//     curHeader.firstChild.className="";
-//   }
-//   else if (curHeader.className=="")
-//   {
-//     curHeader.className="close";
-//     curHeader.firstChild.className="close";
-//   }
+function pagingActivate(name) {
+//  let page
+//  if (curHeader.className === 'close') {
+//    curHeader.className = ''
+//    curHeader.firstChild.className = ''
+//  } else if (curHeader.className === '') {
+//    curHeader.className = 'close'
+//    curHeader.firstChild.className = 'close'
+//  }
 }
